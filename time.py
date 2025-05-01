e3bf4dba80606c48e08e8163f5d25c017db0e809
Masi196 <masihullahw2@gmail.com>
2025-05-01 16:33:27 -0500

    Update time.py

--- time.py
+++ time.py
@@ -1,3 +1,9 @@
+# name: masihullah wardak
+# date 4/28/2025
+# description: this code asks the user current time and wait time and calculte a final waiting time by adding both current and wait time together.
+
+
+
 currentTimeStr = input("What is the current time (in hours 0-23)?")
 
 waitTimeStr = input("How many hours do you want to wait")
