93193c62b88d12e57a90a67b923ab6d0a7a0c716
Masi196 <masihullahw2@gmail.com>
2025-05-01 16:31:03 -0500

    Update pirate.py

--- pirate.py
+++ pirate.py
@@ -1,3 +1,7 @@
+# name: masihullah wardak
+# date 4/28/2025
+# description: this code asks the user a password and based on the password a masssage will be printed to the user.
+
 greeting = input("Hello, possible pirate! What's the password?")
 if greeting == "Arrr!":
 	print("Go away, pirate.")
