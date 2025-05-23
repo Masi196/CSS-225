58144308e70f452f7b33c4485186a9db254ad6d7
Masi196 <masihullahw2@gmail.com>
2025-05-23 12:55:13 -0500

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,13 @@
+# name : masihullah wardak
+# date: 5/23/2025
+# description: this code converts radius to area
+
+
+import math
+
+def areaOfCircle(r):
+    return math.pi * r ** 2
+
+
+r = float(input("enter a number"))
+print(areaOfCircle(r))
